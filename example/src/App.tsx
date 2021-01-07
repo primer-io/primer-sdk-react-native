4df3cfbb5d3e20f38b9dcc5288eebdd374cb5887
StickyCube <will.jk01@gmail.com>
2021-01-08 00:17:27 +0100

    build: update settings

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import ReactNative from '@primer-io/react-native';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
