1540ebd14276b0df4d12458f92a0be2cfeddd7f7
Evangelos Pittas <evangelospittas@gmail.com>
2021-09-13 18:25:33 +0300

    Change payment method to card

--- example/src/usePrimer.ts
+++ example/src/usePrimer.ts
@@ -64,7 +64,7 @@ export function usePrimer() {
     const config = { settings, onTokenizeSuccess };
     const intent: PrimerPaymentMethodIntent = {
       vault: false,
-      paymentMethod: 'Klarna',
+      paymentMethod: 'Card',
     };
 
     Primer.showPaymentMethod(token, intent, config);
