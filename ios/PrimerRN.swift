a82886deeb12b2f80aaabd16acb5161bccc06bde
Evangelos Pittas <evangelospittas@gmail.com>
2021-09-23 15:17:31 +0300

    Add primer delegate

--- ios/PrimerRN.swift
+++ ios/PrimerRN.swift
@@ -151,3 +151,57 @@ class PrimerRN: NSObject {
         
     }
 }
+
+extension PrimerRN: PrimerDelegate {
+
+    func clientTokenCallback(_ completion: @escaping (String?, Error?) -> Void) {
+        guard let clientToken = clientToken else {
+            checkoutFailed(with: ErrorTypeRN.clientTokenNotConfigured)
+            completion(nil, ErrorTypeRN.clientTokenNotConfigured)
+            return
+        }
+        completion(clientToken, nil)
+    }
+    
+    func onTokenizeSuccess(_ paymentMethodToken: PaymentMethodToken, _ completion: @escaping (Error?) -> Void) {
+        do {
+            let json = try encoder.encode(paymentMethodToken)
+            let data = String(data: json, encoding: .utf8)!
+            self.onTokenizeSuccessCallback?([data])
+        } catch {
+            checkoutFailed(with: ErrorTypeRN.ParseJsonFailed)
+        }
+        onResumeFlowCallback = completion
+    }
+    
+    func tokenAddedToVault(_ token: PaymentMethodToken) {
+        do {
+            let json = try encoder.encode(token)
+            let data = String(data: json, encoding: .utf8)!
+            self.onVaultSuccessCallback?([data])
+        } catch {
+            checkoutFailed(with: ErrorTypeRN.ParseJsonFailed)
+        }
+    }
+    
+    func onCheckoutDismissed() {
+        self.onDismissCallback?([])
+    }
+    
+    func checkoutFailed(with error: Error) {
+        do {
+            if let error = error as? ErrorTypeRN {
+                let exception = PrimerErrorRN(exceptionType: error, description: nil)
+                let json = try encoder.encode(exception)
+                let data = String(data: json, encoding: .utf8)!
+                self.onPrimerErrorCallback?([data])
+            } else {
+                self.onPrimerErrorCallback?(["{\"exceptionType\":\"CheckoutFlowFailed\"}"])
+            }
+        } catch {
+            self.onPrimerErrorCallback?(["{\"exceptionType\":\"ParseJsonFailed\"}"])
+        }
+        haltExecution = true
+    }
+}
+
