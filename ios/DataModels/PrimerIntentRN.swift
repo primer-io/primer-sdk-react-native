633e533781bdb7fc8e1d6239482d0dc176c9b267
Carl <carl@primer.io>
2022-01-19 15:26:19 +0000

    update ios intent

--- ios/DataModels/PrimerIntentRN.swift
+++ ios/DataModels/PrimerIntentRN.swift
@@ -2,28 +2,70 @@ import PrimerSDK
 
 struct PrimerIntentRN: Decodable {
     let vault: Bool
-    let paymentMethod: PrimerPaymentMethodTypeRN
+    let paymentMethod: String
     
     var paymentMethodConfigType: PaymentMethodConfigType {
         switch paymentMethod {
-        case .Any:
-            return .paymentCard
-        case .Klarna:
-            return .klarna
-        case .Card:
-            return .paymentCard
-        case .PayPal:
-            return .payPal
-        case .GooglePay:
-            return .googlePay
-        case .ApplePay:
+        case "ADYEN_ALIPAY":
+            return .adyenAlipay
+        case "ADYEN_DOTPAY":
+            return .adyenDotPay
+        case "ADYEN_GIROPAY":
+            return .adyenGiropay
+        case "ADYEN_IDEAL":
+            return .adyenIDeal
+        case "ADYEN_MOBILEPAY":
+            return .adyenMobilePay
+        case "ADYEN_SOFORT":
+            return .adyenSofort
+        case "ADYEN_TRUSTLY":
+            return .adyenTrustly
+        case "ADYEN_TWINT":
+            return .adyenTwint
+        case "ADYEN_VIPPS":
+            return .adyenVipps
+        case "APAYA":
+            return .apaya
+        case "APPLE_PAY":
             return .applePay
-        case .GoCardless:
+        case "ATOME":
+            return .atome
+        case "BUCKAROO_BANCONTACT":
+            return .buckarooBancontact
+        case "BUCKAROO_EPS":
+            return .buckarooEps
+        case "BUCKAROO_GIROPAY":
+            return .buckarooGiropay
+        case "BUCKAROO_IDEAL":
+            return .buckarooIdeal
+        case "BUCKAROO_SOFORT":
+            return .buckarooSofort
+        case "GOCARDLESS":
             return .goCardlessMandate
+        case "GOOGLE_PAY":
+            return .googlePay
+        case "HOOLAH":
+            return .hoolah
+        case "KLARNA":
+            return .klarna
+        case "MOLLIE_BANCONTACT":
+            return .mollieBankcontact
+        case "MOLLIE_IDEAL":
+            return .mollieIdeal
+        case "PAY_NL_BANCONTACT":
+            return .payNLBancontact
+        case "PAY_NL_GIROPAY":
+            return .payNLGiropay
+        case "PAY_NL_IDEAL":
+            return .payNLIdeal
+        case "PAY_NL_PAYCONIQ":
+            return .payNLPayconiq
+        case "PAYMENT_CARD":
+            return .paymentCard
+        case "PAYPAL":
+            return .payPal
+        default:
+            return .other(rawValue: paymentMethod)
         }
     }
-}
-
-enum PrimerPaymentMethodTypeRN: String, Decodable {
-    case `Any`, Klarna, Card, PayPal, GooglePay, ApplePay, GoCardless
 }
