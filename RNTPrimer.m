6b86e16217740403b28395e22f0af870bae02feb
Evangelos Pittas <evangelos@primer.io>
2022-03-16 08:42:00 +0200

    Implement RN native bridge

---
+++ RNTPrimer.m
@@ -0,0 +1,28 @@
+//
+//  RNTPrimer.m
+//  primer-io-react-native
+//
+//  Created by Evangelos on 15/3/22.
+//
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface RCT_EXTERN_MODULE(PrimerNative, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(configureWithSettings:(NSString *)settingsStr)
+
+RCT_EXTERN_METHOD(configureWithTheme:(NSString *)themeStr)
+
+RCT_EXTERN_METHOD(configureWithSettings:(NSString *)settingsStr theme:(NSString *)themeStr)
+
+RCT_EXTERN_METHOD(showUniversalCheckout)
+
+RCT_EXTERN_METHOD(showUniversalCheckoutWithClientToken:(NSString *)clientToken)
+
+RCT_EXTERN_METHOD(setClientToken:(NSString *)clientToken)
+
+RCT_EXTERN_METHOD(setResumeToken:(NSString *)resumeToken)
+
+@end
+
--- RNTPrimer.swift
+++ RNTPrimer.swift
@@ -10,18 +10,24 @@ import PrimerSDK
 
 @objc
 enum PrimerEvents: Int, CaseIterable {
-    case clientTokenCallback = 0
+    case onClientTokenCallback = 0
+    case onError
     
     var stringValue: String {
         switch self {
-        case .clientTokenCallback:
-            return "clientTokenCallback"
+        case .onClientTokenCallback:
+            return "onClientTokenCallback"
+        case .onError:
+            return "onError"
         }
     }
 }
 
-@objc(Primer)
+@objc(PrimerNative)
 class RNTPrimer: RCTEventEmitter {
+    
+    private var clientTokenCallback: ((String?, Error?) -> Void)?
+    private var resumeTokenCallback: ((String?, Error?) -> Void)?
     
     // MARK: - INITIALIZATION & REACT NATIVE SUPPORT
     
@@ -45,21 +51,111 @@ class RNTPrimer: RCTEventEmitter {
     // MARK: - API
     
     @objc
+    public func configureWithSettings(_ settingsStr: String) {
+        do {
+            try self.configure(settingsStr: settingsStr)
+        } catch {
+            self.checkoutFailed(with: error)
+        }
+    }
+    
+    @objc
+    public func configureWithTheme(_ themeStr: String) {
+        do {
+            try self.configure(themeStr: themeStr)
+        } catch {
+            self.checkoutFailed(with: error)
+        }
+    }
+    
+    @objc
+    public func configureWithSettings(_ settingsStr: String, themeStr: String) {
+        do {
+            try self.configure(settingsStr: settingsStr, themeStr: themeStr)
+        } catch {
+            self.checkoutFailed(with: error)
+        }
+    }
+    
+    @objc
     public func showUniversalCheckout() {
-        
+        DispatchQueue.main.async {
+            PrimerSDK.Primer.shared.showUniversalCheckout(on: UIViewController(), clientToken: nil)
+        }
     }
     
     @objc
     public func showUniversalCheckoutWithClientToken(_ clientToken: String) {
+        DispatchQueue.main.async {
+            PrimerSDK.Primer.shared.showUniversalCheckout(on: UIViewController(), clientToken: clientToken)
+        }
+    }
+    
+    @objc
+    public func setClientToken(_ clientToken: String) {
+        self.clientTokenCallback?(clientToken, nil)
+    }
+    
+    @objc
+    public func setResumeToken(_ resumeToken: String) {
+        self.resumeTokenCallback?(resumeToken, nil)
+    }
+    
+    @objc
+    public func setError(_ errorStr: String) {
         
     }
     
+    // MARK: - HELPERS
+    
+    private func configure(settingsStr: String? = nil, themeStr: String? = nil) throws {
+        var settings: PrimerSettings?
+        if let settingsStr = settingsStr {
+            guard let settingsData = settingsStr.data(using: .utf8) else {
+                return
+            }
+            let settingsRN = try JSONDecoder().decode(PrimerSettingsRN.self, from: settingsData)
+            settings = settingsRN.asPrimerSettings()
+        }
+        
+        var theme: PrimerTheme?
+        if let themeStr = themeStr {
+            guard let themeData = themeStr.data(using: .utf8) else {
+                return
+            }
+            let themeRN = try JSONDecoder().decode(PrimerThemeRN.self, from: themeData)
+            theme = themeRN.asPrimerTheme()
+        }
+        
+        
+        DispatchQueue.main.async {
+            PrimerSDK.Primer.shared.configure(settings: settings, theme: theme)
+        }
+    }
+    
+    private func callActiveCallbackWithError(_ error: Error) {
+        self.clientTokenCallback?(nil, error)
+        self.resumeTokenCallback?(nil, error)
+    }
+    
+    private func removeCallbacks() {
+        self.clientTokenCallback = nil
+        self.resumeTokenCallback = nil
+    }
 }
 
 extension RNTPrimer: PrimerDelegate {
     
     func clientTokenCallback(_ completion: @escaping (String?, Error?) -> Void) {
-        
+        self.clientTokenCallback = { (clientToken, err) in
+            completion(clientToken, err)
+            self.removeCallbacks()
+        }
+        sendEvent(withName: PrimerEvents.onClientTokenCallback.stringValue, body: nil)
+    }
+    
+    func checkoutFailed(with error: Error) {
+        sendEvent(withName: PrimerEvents.onError.stringValue, body: nil)
     }
     
 }
