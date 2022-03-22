773c5871f14433eb334f1231e9180229e9ffb2d4
Evangelos Pittas <evangelos@primer.io>
2022-03-22 12:10:48 +0200

    Refactor RN bridge, keep old one as proxy in order to not modify the merchant facing API.

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,6 @@
-import Primer from './Primer';
+import type { IPrimer } from './models/primer';
+import { PrimerNativeMapping } from './Primer';
+
+const Primer: IPrimer = PrimerNativeMapping;
+
 export { Primer };
