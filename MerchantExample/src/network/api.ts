6dc3e9349fca9038cd8ce8dcc7ca8aa3ec2b77c1
Evangelos Pittas <evangelos@primer.io>
2022-03-24 20:21:21 +0200

    Add axios

---
+++ MerchantExample/src/network/api.ts
@@ -0,0 +1,12 @@
+
+export const createClientSession = async (clientSessionData: any) => {
+
+}
+
+export const createPayment = async (paymentData: any) => {
+
+}
+
+export const resumePayment = async (paymentId: string, resumeToken: string) => {
+
+}
