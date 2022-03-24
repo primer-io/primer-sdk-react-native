e4e73711181837cf90fb921982ea0e6f62b27a3e
Evangelos Pittas <evangelos@primer.io>
2022-03-24 09:41:21 +0200

    Add react navigation

---
+++ MerchantExample/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import SettingsScreen from './screens/SettingsScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <NavigationContainer>
+        <SettingsScreen/>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+export default App;
