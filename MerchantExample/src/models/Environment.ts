425254d8e57b70ec4a8852f4c5a3576266779755
Evangelos Pittas <evangelos@primer.io>
2022-03-28 10:23:15 +0300

    Refactor

--- MerchantExample/src/models/Environment.ts
+++ MerchantExample/src/models/Environment.ts
@@ -4,3 +4,48 @@ export enum Environment {
     Staging,
     Production,
 }
+
+export function getEnvironmentStringVal(env: Environment): string {
+    switch (env) {
+    case Environment.Dev:
+        return "dev"
+    case Environment.Sandbox:
+        return "sandbox"
+    case Environment.Staging:
+        return "staging"
+    case Environment.Production:
+        return "production"
+    default:
+            return "unknown"
+    }
+}
+
+export function makeEnvironmentFromStringVal(env: string): Environment {
+    switch (env) {
+    case "dev":
+        return Environment.Dev;
+    case "sandbox":
+        return Environment.Sandbox;
+    case "Staging":
+        return Environment.Staging;
+    case "production":
+        return Environment.Production;
+    default:
+        throw new Error("Failed to create environment.");
+    }
+}
+
+export function makeEnvironmentFromIntVal(env: number): Environment {
+    switch (env) {
+    case 0:
+        return Environment.Dev;
+    case 1:
+        return Environment.Sandbox;
+    case 2:
+        return Environment.Staging;
+    case 3:
+        return Environment.Production;
+    default:
+        throw new Error("Failed to create environment.");
+    }
+}
--- MerchantExample/src/network/api.ts
+++ MerchantExample/src/network/api.ts
@@ -1,12 +1,82 @@
+import axios from 'axios';
+import { environment } from '../screens/SettingsScreen';
+import { getEnvironmentStringVal } from '../models/Environment';
+import { IClientSessionRequestBody } from '../models/IClientSessionRequestBody';
 
-export const createClientSession = async (clientSessionData: any) => {
+const baseUrl = 'https://us-central1-primerdemo-8741b.cloudfunctions.net/api';
 
+let staticHeaders = {
+    'Content-Type': 'application/json',
+    'environment': getEnvironmentStringVal(environment),
 }
 
-export const createPayment = async (paymentData: any) => {
+export const createClientSession = async (body: IClientSessionRequestBody) => {
+    const url = baseUrl + '/client-session';
+    const headers = { ...staticHeaders, 'X-Api-Version': '2021-10-19' };
 
+    try {
+        console.log(`\nREQUEST:`);
+        console.log(url);
+        console.log(`\nHEADERS:`)
+        console.log(headers);
+        console.log(`\nBODY:`);
+        console.log(body);
+        console.log(`\n`);
+        const response = await axios.post(url, body, { headers: headers });
+
+        if (response.status >= 200 && response.status < 300) {
+            return response.data;
+        } else {
+            const err = new Error(`Request failed with status ${response.status}.\nBody: ${JSON.stringify(response.data)}`);
+            console.error(err);
+            throw err;
+        }
+    } catch (err) {
+        console.log(err.response.data);
+        console.error(err);
+        throw err;
+    }
 }
+
+export const createPayment = async (paymentMethodToken: string) => {
+    const url = baseUrl + '/payments';
+    const headers = { ...staticHeaders, 'X-Api-Version': '2021-10-19' };
+
+    const body = { paymentMethodToken: paymentMethodToken };
+    try {
+        const response = await axios.post(url, body, { headers: headers });
+
+        if (response.status >= 200 && response.status < 300) {
+            return response.data;
+        } else {
+            const err = new Error(`Request failed with status ${response.status}.\nBody: ${JSON.stringify(response.data)}`);
+            console.error(err);
+            throw err;
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
 
 export const resumePayment = async (paymentId: string, resumeToken: string) => {
+    const url = baseUrl + `/payments/${paymentId}/resume`;
+    const headers = { ...staticHeaders, 'X-Api-Version': '2021-10-19' };
 
+    const body = { resumeToken: resumeToken };
+
+    try {
+        const response = await axios.post(url, body, { headers: headers });
+
+        if (response.status >= 200 && response.status < 300) {
+            return response.data;
+        } else {
+            const err = new Error(`Request failed with status ${response.status}.\nBody: ${JSON.stringify(response.data)}`);
+            console.error(err);
+            throw err;
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 }
