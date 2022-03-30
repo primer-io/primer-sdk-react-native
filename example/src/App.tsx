04627aa2a070818b1c10d3bfe6fa435445585470
Evangelos Pittas <evangelos@primer.io>
2022-03-30 17:10:10 +0300

    Add example RN app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
+import { useColorScheme } from 'react-native';
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SettingsScreen from './screens/SettingsScreen';
 import CheckoutScreen from './screens/CheckoutScreen';
+import ResultScreen from './screens/ResultScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +22,7 @@ const App = () => {
         <Stack.Navigator>
           <Stack.Screen name="Settings" component={SettingsScreen} />
           <Stack.Screen name="Checkout" component={CheckoutScreen} />
+          <Stack.Screen name="Result" component={ResultScreen} />
         </Stack.Navigator>
       </NavigationContainer>
   );
