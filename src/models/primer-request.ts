d52b22e3f6a0ee1558c648d2e3774d37c1b40c61
Evangelos Pittas <evangelos@primer.io>
2022-05-18 10:30:39 +0300

    Remove unused code

--- src/models/primer-request.ts
+++
@@ -1,7 +0,0 @@
-export type PrimerResumeHandler = IPrimerResumeHandler;
-
-interface IPrimerResumeHandler {
-  handleNewClientToken: (clientToken: string) => void;
-  handleError: (error: Error) => void;
-  handleSuccess: () => void;
-}
--- src/models/utils/redirectScheme.ts
+++
@@ -1,5 +0,0 @@
-var redirectSchemes = [
-    'primer'
-] as const;
-
-export type RedirectScheme = typeof redirectSchemes[number];
