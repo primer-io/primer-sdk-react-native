52aee67e14fc838ea8661ef6ca05e9d53bd6e03d
Evangelos Pittas <evangelos@primer.io>
2022-05-18 10:31:10 +0300

    Refactor SDK API

--- src/models/IPrimer.ts
+++ src/models/IPrimer.ts
@@ -1,20 +1,28 @@
+import type { IPrimerCheckoutData } from "./IPrimerCheckoutData";
+import type { IPrimerCheckoutPaymentMethodData } from "./IPrimerCheckoutPaymentMethodData";
+import type { IPrimerClientSession } from "./IPrimerClientSession";
+import type { PrimerSessionIntent } from "./PrimerSessionIntent";
+import type { PrimerPaymentMethodTokenData } from "./PrimerPaymentMethodTokenData";
+import type { PrimerSettings } from "./PrimerSettings";
+
 export interface IPrimer {
+  configure(settings: PrimerSettings): Promise<void>;
   showUniversalCheckout(clientToken: string): Promise<void>;
   showVaultManager(clientToken: string): Promise<void>;
   showPaymentMethod(
     paymentMethodType: string,
-    intent: 'CHECKOUT' | 'VAULT',
+    intent: PrimerSessionIntent,
     clientToken: string
   ): Promise<void>
   dismiss(): void;
 
   onBeforeClientSessionUpdate?: () => void;
-  onClientSessionUpdate?: (clientSession: any) => void;
-  onBeforePaymentCreate?: (paymentMethodTokenData: any, handler: PaymentCreationHandler) => void;
-  onCheckoutComplete?: (checkoutData: any) => void;
+  onClientSessionUpdate?: (clientSession: IPrimerClientSession) => void;
+  onBeforePaymentCreate?: (checkoutPaymentMethodData: IPrimerCheckoutPaymentMethodData, handler: PaymentCreationHandler) => void;
+  onCheckoutComplete?: (checkoutData: IPrimerCheckoutData) => void;
   onCheckoutFail?: (error: Error, handler: ErrorHandler) => void;
-  onTokenizeSuccess?: (paymentMethodTokenData: any, handler: TokenizationHandler) => void;
-  onResumeSuccess?: (resumeTokenData: any, handler: ResumeHandler) => void;
+  onTokenizeSuccess?: (paymentMethodTokenData: PrimerPaymentMethodTokenData, handler: TokenizationHandler) => void;
+  onResumeSuccess?: (resumeToken: string, handler: ResumeHandler) => void;
 }
 
 export interface PaymentCreationHandler {
