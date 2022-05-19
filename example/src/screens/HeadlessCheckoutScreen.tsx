4d98e1a734323d39eae449661a785c85277ba87a
Evangelos Pittas <evangelos@primer.io>
2022-05-19 15:53:26 +0300

    Bump example app

--- example/src/screens/HeadlessCheckoutScreen.tsx
+++ example/src/screens/HeadlessCheckoutScreen.tsx
@@ -38,6 +38,9 @@ export const HeadlessCheckoutScreen = () => {
           merchantIdentifier: 'merchant.checkout.team',
         },
       },
+      business: {
+        name: "Business Name"
+      },
       onPreparationStarted: () => {
         console.log(`merchant app | onPreparationStarted`);
       },
@@ -117,7 +120,83 @@ export const HeadlessCheckoutScreen = () => {
   }, []);
 
   const payWithPaymentMethod = (paymentMethod: string) => {
-    HeadlessUniversalCheckout.showPaymentMethod(paymentMethod);
+    const settings: PrimerSettings &
+      PrimerHeadlessUniversalCheckoutCallbacks = {
+      options: {
+        ios: {
+          merchantIdentifier: 'merchant.checkout.team',
+        },
+      },
+      business: {
+        name: "Business Name"
+      },
+      onPreparationStarted: () => {
+        console.log(`merchant app | onPreparationStarted`);
+      },
+      onTokenizeStart: () => {
+        console.log(`merchant app | onTokenizeStart`);
+      },
+      onTokenizeSuccess: async (paymentMethodInstrument) => {
+        console.log(`merchant app | onTokenizeSuccess`);
+        try {
+          const response = await createPayment(paymentMethodInstrument.token);
+          console.log(JSON.stringify(response));
+          if (
+            response.id &&
+            response.requiredAction &&
+            response.requiredAction.clientToken
+          ) {
+            paymentId.current = response.id;
+            HeadlessUniversalCheckout.resumeWithClientToken(
+              response.requiredAction.clientToken
+            );
+          }
+        } catch (error) {
+          console.error(error);
+          setError(error);
+        }
+      },
+      onPaymentMethodPresented: () => {
+        console.log(`merchant app | onPaymentMethodPresented`);
+      },
+      onResumeSuccess: async (resumeToken) => {
+        console.log(`merchant app | onResumeSuccess`);
+
+        try {
+          if (paymentId.current) {
+            const response = await resumePayment(
+              paymentId.current,
+              resumeToken
+            );
+            setPaymentResponse(response);
+          } else {
+            const err = new Error('Missing payment id');
+            throw err;
+          }
+        } catch (err) {
+          console.error(err);
+          setError(err);
+        }
+      },
+      onFailure: (err) => {
+        console.log(`merchant app | onFailure`);
+      },
+    };
+
+    createClientSession().then((session) => {
+      setIsLoading(false);
+      HeadlessUniversalCheckout.startWithClientToken(
+        session.clientToken,
+        settings
+      )
+        .then((response) => {
+          HeadlessUniversalCheckout.showPaymentMethod(paymentMethod);
+        })
+        .catch((err) => {
+          console.error(err);
+          setError(err);
+        });
+    });
   };
 
   const renderPaymentMethods = () => {
