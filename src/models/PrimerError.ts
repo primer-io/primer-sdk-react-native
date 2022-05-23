866193a76bbd5c446215292c975ab07381fece7e
Evangelos Pittas <evangelos@primer.io>
2022-05-23 10:23:58 +0300

    Rename and refactor classes

---
+++ src/models/PrimerError.ts
@@ -0,0 +1,23 @@
+export interface IPrimerError {
+  errorId: string;
+  description?: string;
+  recoverySuggestion?: string;
+}
+
+
+export class PrimerError extends Error {
+
+  errorId: string;
+  description: string;
+  recoverySuggestion?: string;
+
+  constructor(errorId: string, description: string, recoverySuggestion: string | undefined) {
+      super(description);
+      this.errorId = errorId;
+      this.description = description;
+      this.recoverySuggestion = recoverySuggestion;
+
+      Object.setPrototypeOf(this, PrimerError.prototype);
+  }
+
+}
