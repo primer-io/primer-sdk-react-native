b017894225ec65e0798b9283d30542b32e7428e7
Dario Carlomagno <dario@primer.io>
2022-08-29 11:44:19 +0200

    Removed duplicated Bridging Header file

--- ios/ReactNative-Bridging-Header.h
+++ ios/ReactNative-Bridging-Header.h
@@ -2,3 +2,13 @@
 #import <React/RCTEventEmitter.h>
 #import <React/RCTViewManager.h>
 #import <React/RCTUIManager.h>
+#import <React/RCTView.h>
+
+// for view controller implementation https://stackoverflow.com/questions/45741903/react-native-ios-how-can-i-navigate-to-a-non-react-native-view-native-ios-vie/46007680#46007680
+//#import "React/RCTBridge.h"
+//#import "React/RCTEventDispatcher.h"
+//#import "React/RCTRootView.h"
+//#import "React/RCTUtils.h"
+//#import "React/RCTConvert.h"
+//#import "React/RCTBundleURLProvider.h"
+//#import "RCTViewManager.h"
