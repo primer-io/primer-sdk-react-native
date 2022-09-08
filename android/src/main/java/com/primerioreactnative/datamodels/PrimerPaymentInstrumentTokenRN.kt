47eed7ec0e1139b7881ab796345a6d855de07c83
semirp <85510694+semirp@users.noreply.github.com>
2022-09-08 13:14:35 +0200

    Handled new serialization updates on native (#107)
    * Handled new serialization updates on native

--- android/src/main/java/com/primerioreactnative/datamodels/PrimerPaymentInstrumentTokenRN.kt
+++ android/src/main/java/com/primerioreactnative/datamodels/PrimerPaymentInstrumentTokenRN.kt
@@ -1,7 +1,7 @@
 package com.primerioreactnative.datamodels
 
-import io.primer.android.data.tokenization.models.PaymentInstrumentData
-import io.primer.android.data.tokenization.models.TokenType
+import com.primerioreactnative.extensions.toPaymentInstrumentDataRN
+import io.primer.android.data.tokenization.models.*
 import io.primer.android.domain.tokenization.models.PrimerPaymentMethodTokenData
 import kotlinx.serialization.Serializable
 
@@ -22,12 +22,58 @@ data class PrimerPaymentInstrumentTokenRN(
   )
 
   @Serializable
-  data class ThreeDSAuthenticationData (
+  data class ThreeDSAuthenticationData(
     val responseCode: String?,
     val reasonCode: String?,
     val reasonText: String?,
     val protocolVersion: String?,
     val challengeIssued: Boolean?,
+  )
+
+  @Serializable
+  data class PaymentInstrumentData(
+    val network: String? = null,
+    val cardholderName: String? = null,
+    val first6Digits: Int? = null,
+    val last4Digits: Int? = null,
+    val expirationMonth: Int? = null,
+    val expirationYear: Int? = null,
+    val gocardlessMandateId: String? = null,
+    val externalPayerInfo: ExternalPayerInfo? = null,
+    val klarnaCustomerToken: String? = null,
+    val sessionData: SessionData? = null,
+    // apaya
+    val mx: String? = null,
+    val mnc: Int? = null,
+    val mcc: Int? = null,
+    val hashedIdentifier: String? = null,
+    val currencyCode: String? = null,
+    val productId: String? = null,
+    // async
+    val paymentMethodType: String? = null,
+    // bin
+    val binData: BinData? = null,
+  )
+
+  @Serializable
+  data class ExternalPayerInfo(
+    val email: String,
+  )
+
+  @Serializable
+  data class BinData(
+    val network: String? = null,
+  )
+
+  @Serializable
+  data class SessionData(
+    val recurringDescription: String? = null,
+    val billingAddress: BillingAddress? = null,
+  )
+
+  @Serializable
+  data class BillingAddress(
+    val email: String,
   )
 
   companion object {
@@ -37,7 +83,7 @@ data class PrimerPaymentInstrumentTokenRN(
         token.analyticsId,
         token.tokenType,
         token.paymentInstrumentType,
-        token.paymentInstrumentData,
+        token.paymentInstrumentData?.toPaymentInstrumentDataRN(),
       )
     }
   }
---
+++ android/src/main/java/com/primerioreactnative/extensions/PrimerPaymentMethodOptionsRN.kt
@@ -0,0 +1,27 @@
+package com.primerioreactnative.extensions
+
+import com.primerioreactnative.datamodels.*
+import io.primer.android.data.settings.*
+
+fun PrimerPaymentMethodOptionsRN.toPrimerPaymentMethodOptions() = PrimerPaymentMethodOptions(
+  androidSettingsRN.redirectScheme,
+  cardPaymentOptions.toPrimerCardPaymentOptions(),
+  googlePayOptions.toPrimerGooglePayOptions(),
+  klarnaOptions.toPrimerKlarnaOptions(),
+  apayaOptions.toPrimerApayaOptions(),
+  goCardlessOptions.toPrimerGoCardlessOptions()
+)
+
+fun PrimerCardPaymentOptionsRN.toPrimerCardPaymentOptions() =
+  PrimerCardPaymentOptions(is3DSOnVaultingEnabled)
+
+fun PrimerGooglePayOptionsRN.toPrimerGooglePayOptions() =
+  PrimerGooglePayOptions(merchantName, allowedCardNetworks, buttonStyle)
+
+fun PrimerKlarnaOptionsRN.toPrimerKlarnaOptions() =
+  PrimerKlarnaOptions(recurringPaymentDescription, webViewTitle)
+
+fun PrimerApayaOptionsRN.toPrimerApayaOptions() = PrimerApayaOptions(webViewTitle)
+
+fun PrimerGoCardlessOptionsRN.toPrimerGoCardlessOptions() =
+  PrimerGoCardlessOptions(businessName, businessAddress)
