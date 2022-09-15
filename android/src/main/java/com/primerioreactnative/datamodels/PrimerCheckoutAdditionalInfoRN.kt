fabba2b35b71c56d88d8b488c47053972145ac62
Dario Carlomagno <dario@primer.io>
2022-09-15 12:17:13 +0200

    Added Kotlin model

--- android/src/main/java/com/primerioreactnative/datamodels/PrimerCheckoutAdditionalInfoRN.kt
+++ android/src/main/java/com/primerioreactnative/datamodels/PrimerCheckoutAdditionalInfoRN.kt
@@ -11,3 +11,12 @@ data class MultibancoCheckoutAdditionalInfoRN(
   val reference: String,
   val entity: String,
 ) : PrimerCheckoutAdditionalInfoRN()
+
+interface PrimerCheckoutQRCodeInfoRN : PrimerCheckoutAdditionalInfoRN
+
+@Serializable
+data class PromptPayCheckoutAdditionalInfoRN(
+  val expiresAt: String,
+  val qrCodeUrl: String?,
+  val qrCodeBase64: String?,
+) : PrimerCheckoutQRCodeInfoRN()
