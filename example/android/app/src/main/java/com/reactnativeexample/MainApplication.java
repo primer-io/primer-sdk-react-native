afe9458f48baa95194fbd687f4c0573731a383bd
Semir <semir@primer.io>
2022-11-08 12:47:00 +0100

    Fixed Android app

--- example/android/app/src/main/java/com/reactnativeexample/MainApplication.java
+++ example/android/app/src/main/java/com/reactnativeexample/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
+import com.primerioreactnative.ReactNativePackage;
 import com.reactnativeexample.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -27,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new ReactNativePackage());
           return packages;
         }
 
