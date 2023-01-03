0d3070aa9072e847f04c8efdd5b13d4700f11eb7
Semir <semir@primer.io>
2023-01-03 10:36:24 +0100

    Integrated SDK with the new example app

--- example_0_70_6/android/app/src/main/java/com/example_0_70_6/MainApplication.java
+++ example_0_70_6/android/app/src/main/java/com/example_0_70_6/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
 import com.example_0_70_6.newarchitecture.MainApplicationReactNativeHost;
+import com.primerioreactnative.ReactNativePackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -27,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new ReactNativePackage());
           return packages;
         }
 
