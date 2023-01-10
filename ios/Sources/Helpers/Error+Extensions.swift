8733622347ab65cec2da859e72bc9c129c7e3431
Evangelos Pittas <evangelos@primer.io>
2023-01-10 14:20:21 +0200

    Pass `errorId`

--- ios/Sources/Helpers/Error+Extensions.swift
+++ ios/Sources/Helpers/Error+Extensions.swift
@@ -6,13 +6,25 @@
 //
 
 import Foundation
+import PrimerSDK
 
 internal extension Error {
     var rnError: [String: String] {
         var json: [String: String] = [
-            "errorId": (self as NSError).domain,
             "description": localizedDescription
         ]
+        
+        if let primerValidationErr = self as? PrimerValidationError {
+            json["errorId"] = primerValidationErr.errorId
+            json["diagnosticsId"] = primerValidationErr.diagnosticsId
+            
+        } else if let primerErr = self as? PrimerError {
+            json["errorId"] = primerErr.errorId
+            json["diagnosticsId"] = primerErr.diagnosticsId
+            
+        } else {
+            json["errorId"] = (self as NSError).domain
+        }
         
         if let inputElementType = (self as NSError).userInfo["inputElementType"] as? String {
             json["inputElementType"] = inputElementType
