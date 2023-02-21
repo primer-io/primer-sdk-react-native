3d491c9bdd58ece5fc75c13de3666a31d0a90799
Aladin Taleb <aladin.taleb@gmail.com>
2023-02-21 15:03:37 +0100

    pass capture billing address enabled to react native

--- ios/Sources/DataModels/PrimerSettings+Extensions.swift
+++ ios/Sources/DataModels/PrimerSettings+Extensions.swift
@@ -35,8 +35,10 @@ extension PrimerSettings {
             var applePayOptions: PrimerApplePayOptions?
             if let rnApplePayOptions = ((settingsJson["paymentMethodOptions"] as? [String: Any])?["applePayOptions"] as? [String: Any]),
                let rnApplePayMerchantIdentifier = rnApplePayOptions["merchantIdentifier"] as? String,
-               let rnApplePayMerchantName = rnApplePayOptions["merchantName"] as? String {
-                applePayOptions = PrimerApplePayOptions(merchantIdentifier: rnApplePayMerchantIdentifier, merchantName: rnApplePayMerchantName)
+               let rnApplePayMerchantName = rnApplePayOptions["merchantName"] as? String,
+               let rnApplePayIsCaptureBillingAddressEnabled = rnApplePayOptions["isCaptureBillingAddressEnabled"] as? String
+               {
+                applePayOptions = PrimerApplePayOptions(merchantIdentifier: rnApplePayMerchantIdentifier, merchantName: rnApplePayMerchantName, isCaptureBillingAdressEnabled: rnApplePayIsCaptureBillingAddressEnabled)
             }
 
             var klarnaOptions: PrimerKlarnaOptions?
