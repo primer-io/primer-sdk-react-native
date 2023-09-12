45165a0778bee18bf96baa3a3233f761996af7d1
Jack Newcombe <jack.newcombe@primer.io>
2023-09-12 10:30:26 +0100

    Remove TODOs for removeListener as it is not part of public interface for android or iOS

--- src/RNPrimer.ts
+++ src/RNPrimer.ts
@@ -1,5 +1,6 @@
 import { NativeEventEmitter, NativeModules, EmitterSubscription} from 'react-native';
 import type { PrimerSettings } from './models/PrimerSettings';
+import { EventSubscription } from 'react-native';
 
 const { NativePrimer } = NativeModules;
 const eventEmitter = new NativeEventEmitter(NativePrimer);
@@ -42,12 +43,12 @@ const RNPrimer = {
   // Event Emitter
   ///////////////////////////////////////////
 
-  addListener: (eventType: EventType, listener: (...args: any[]) => any) => {
-    eventEmitter.addListener(eventType, listener);
+  addListener(eventType: EventType, listener: (...args: any[]) => any): EventSubscription {
+    return eventEmitter.addListener(eventType, listener);
   },
 
-  removeListener(subscription: EmitterSubscription): void {
-    return subscription.remove();
+  removeListener(subscription: EmitterSubscription) {
+    subscription.remove();
   },
 
   removeAllListenersForEvent(eventType: EventType) {
