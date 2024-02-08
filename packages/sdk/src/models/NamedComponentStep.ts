89d570fb4a93831b8782f3cb183acaffb7045277
FlaviuDuncaTW <flaviu@ext.primer.io>
2024-02-08 13:49:08 +0200

    chore: move NamedComponentStep to its own file

---
+++ packages/sdk/src/models/NamedComponentStep.ts
@@ -0,0 +1,11 @@
+export type NamedComponentStep = INamedComponentStep;
+
+/**
+ * A component step that can only be identified via its name due to its lack of properties.
+ */
+interface INamedComponentStep {
+    /**
+     * The name of this component step.
+     */
+    name: string
+}
--- packages/sdk/src/models/componentWithRedirect/ComponentWithRedirectCollectableData.ts
+++ packages/sdk/src/models/componentWithRedirect/ComponentWithRedirectCollectableData.ts
@@ -1,6 +1,5 @@
 export type BankId = IBankId;
 export type BankListFilter = IBankListFilter;
-export type NamedComponentStep = INamedComponentStep;
 
 /**
  * Interface representing the id of the selected bank.
@@ -20,15 +19,4 @@ interface IBankListFilter {
      * The text to filter the bank list by.
      */
     text: string;
-}
-
-
-/**
- * A component step that can only be identified via its name due to its lack of properties.
- */
-interface INamedComponentStep {
-    /**
-     * The name of this component step.
-     */
-    name: string
 }
