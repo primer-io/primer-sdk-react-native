bd6a3f8ef11852603781c58c508bd3054f990f19
FlaviuDuncaTW <flaviu@ext.primer.io>
2024-02-12 17:45:16 +0200

    chore: use lowercase step name

--- packages/sdk/src/models/klarna/KlarnaPaymentSteps.ts
+++ packages/sdk/src/models/klarna/KlarnaPaymentSteps.ts
@@ -2,9 +2,8 @@ import { KlarnaPaymentCategory } from "./KlarnaPaymentCategory"
 
 export type PaymentSessionCreated = IPaymentSessionCreated;
 export type PaymentSessionAuthorized = IPaymentSessionAuthorized;
-
 export type PaymentSessionFinalized = { "name": "paymentSessionFinalized" };
-export type PaymentViewLoaded = { "name": "PaymentViewLoaded" };
+export type PaymentViewLoaded = { "name": "paymentViewLoaded" };
 
 interface IPaymentSessionCreated {
     paymentCategories: KlarnaPaymentCategory[];
