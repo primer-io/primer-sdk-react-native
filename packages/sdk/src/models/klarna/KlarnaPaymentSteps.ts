541ec8817bbb8ac4287a9616af1ba6150aedb914
FlaviuDuncaTW <flaviu@ext.primer.io>
2024-02-20 23:24:49 +0200

    chore: add JDoc to Klarna payment steps

--- packages/sdk/src/models/klarna/KlarnaPaymentSteps.ts
+++ packages/sdk/src/models/klarna/KlarnaPaymentSteps.ts
@@ -1,8 +1,23 @@
 import { KlarnaPaymentCategory } from "./KlarnaPaymentCategory"
 
+/**
+ * A type representing the created payment session.
+ */
 export type PaymentSessionCreated = IPaymentSessionCreated;
+
+/**
+ * A type representing the authorized payment session.
+ */
 export type PaymentSessionAuthorized = IPaymentSessionAuthorized;
+
+/**
+ * A type representing the finalized payment session.
+ */
 export type PaymentSessionFinalized = { "name": "paymentSessionFinalized" };
+
+/**
+ * A type that indicates that the payment view is loaded and ready to be displayed.
+ */
 export type PaymentViewLoaded = { "name": "paymentViewLoaded" };
 
 interface IPaymentSessionCreated {
