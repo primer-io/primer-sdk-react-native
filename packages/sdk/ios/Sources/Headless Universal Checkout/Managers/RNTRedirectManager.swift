c727ea5fbbc21beb49e7153e44f65fd3e2d401fe
StefanV-PRIMERIO <stefan@ext.primer.io>
2024-03-27 21:49:38 +0200

    chore: Fix pod dependencies and align with android

--- packages/sdk/ios/Sources/Headless Universal Checkout/Managers/RNTRedirectManager.swift
+++ packages/sdk/ios/Sources/Headless Universal Checkout/Managers/RNTRedirectManager.swift
@@ -8,206 +8,176 @@
 import Foundation
 import PrimerSDK
 
-@objc
-enum RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents: Int, CaseIterable {
-  
-  case onRetrieved = 0
-  case onRetrieving=1
-  case onError=2
-  case onValid=3
-  case onInvalid=4
-  case onValidating
-  
-  var stringValue: String {
-    switch self {
-    case .onRetrieved:
-      return "onRetrieved"
-    case .onRetrieving:
-      return "onRetrieving"
-    case .onError:
-      return "onError"
-    case .onValid:
-      return "onValid"
-    case .onInvalid:
-      return "onInvalid"
-    case .onValidating:
-      return "onValidating"
-    }
-  }
-}
-  
-  
-  @objc(RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManager)
-  class RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManager: RCTEventEmitter {
+@objc(RNTPrimerHeadlessUniversalCheckoutBanksComponent)
+class RNTPrimerHeadlessUniversalCheckoutBanksComponent: RCTEventEmitter {
     
     private var redirectManager: PrimerHeadlessUniversalCheckout.ComponentWithRedirectManager!
     var banksComponent: (any BanksComponent)?
-
+    
     override class func requiresMainQueueSetup() -> Bool {
         return true
     }
-
+    
     override init() {
-      super.init()
+        super.init()
     }
     
-      override func supportedEvents() -> [String] {
-      return RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.allCases.compactMap({ $0.stringValue })
+    override func supportedEvents() -> [String] {
+        return PrimerHeadlessUniversalCheckoutComponentEvent.allCases.compactMap({ $0.stringValue })
     }
     
     // MARK: - API
     
     @objc
     public func configure(
-      _ paymentMethodTypeStr: String,
-      resolver: @escaping RCTPromiseResolveBlock,
-      rejecter: @escaping RCTPromiseRejectBlock
+        _ paymentMethodTypeStr: String,
+        resolver: @escaping RCTPromiseResolveBlock,
+        rejecter: @escaping RCTPromiseRejectBlock
     ) {
-      print("configure")
-      do {
-        redirectManager = PrimerHeadlessUniversalCheckout.ComponentWithRedirectManager()
-        
-        if #available(iOS 13.0, *) {
-          guard let banksComponent: any BanksComponent = try? redirectManager.provide(paymentMethodType: paymentMethodTypeStr) else {
-            let err = RNTNativeError(
-                                errorId: "native-ios",
-                                errorDescription: "Failed to find asset of \(paymentMethodTypeStr) for this session",
-                                recoverySuggestion: nil)
-                            throw err
-          }
+        print("configure")
+        do {
+            redirectManager = PrimerHeadlessUniversalCheckout.ComponentWithRedirectManager()
 
-          banksComponent.start()
-          self.banksComponent = banksComponent
-
-        } else {
-            guard let banksComponent: any BanksComponent = try? redirectManager.provideBanksComponent(paymentMethodType: paymentMethodTypeStr) as? any BanksComponent else {
-              let err = RNTNativeError(
-                                  errorId: "native-ios",
-                                  errorDescription: "Failed to find asset of \(paymentMethodTypeStr) for this session",
-                                  recoverySuggestion: nil)
-                              throw err
+            guard let banksComponent: any BanksComponent = try? redirectManager.provide(paymentMethodType: paymentMethodTypeStr) else {
+                let err = RNTNativeError(
+                    errorId: "native-ios",
+                    errorDescription: "Failed to find asset of \(paymentMethodTypeStr) for this session",
+                    recoverySuggestion: nil)
+                throw err
             }
 
-            banksComponent.start()
             self.banksComponent = banksComponent
+
+            banksComponent.stepDelegate = self
+            banksComponent.validationDelegate = self
+            banksComponent.errorDelegate = self
+
+            resolver(nil)
+
+        } catch {
+            rejecter(error.rnError["errorId"]!, error.rnError["description"], error)
         }
-        
-        banksComponent?.stepDelegate = self
-        banksComponent?.validationDelegate = self
-        banksComponent?.errorDelegate = self
-        
-        resolver(nil)
-        
-      } catch {
-        rejecter(error.rnError["errorId"]!, error.rnError["description"], error)
-      }
     }
-    
+
     @objc
     public func cleanUp(
-      _ resolver: RCTPromiseResolveBlock,
-      rejecter: RCTPromiseRejectBlock
+        _ resolver: RCTPromiseResolveBlock,
+        rejecter: RCTPromiseRejectBlock
     ) {
         self.banksComponent = nil
         resolver(nil)
     }
-
+    
+    @objc
+    public func start(
+        _ resolver: RCTPromiseResolveBlock,
+        rejecter: RCTPromiseRejectBlock
+    ) {
+        banksComponent?.start()
+        resolver(nil)
+    }
+    
     @objc
     public func submit(
-      _ resolver: RCTPromiseResolveBlock,
-      rejecter: RCTPromiseRejectBlock
+        _ resolver: RCTPromiseResolveBlock,
+        rejecter: RCTPromiseRejectBlock
     ) {
-      banksComponent?.submit()
-      resolver(nil)
+        banksComponent?.submit()
+        resolver(nil)
     }
     
     @objc
     public func onBankFilterChange(
-      _ filterText: String,
-      resolver: RCTPromiseResolveBlock,
-      rejecter: RCTPromiseRejectBlock
+        _ filterText: String,
+        resolver: RCTPromiseResolveBlock,
+        rejecter: RCTPromiseRejectBlock
     ) {
-      banksComponent?.updateCollectedData(collectableData: BanksCollectableData.bankFilterText(text: filterText))
-      resolver(nil)
+        banksComponent?.updateCollectedData(collectableData: BanksCollectableData.bankFilterText(text: filterText))
+        resolver(nil)
     }
     
     @objc
     public func onBankSelected(
-      _ bankId: String,
-      resolver: RCTPromiseResolveBlock,
-      rejecter: RCTPromiseRejectBlock
+        _ bankId: String,
+        resolver: RCTPromiseResolveBlock,
+        rejecter: RCTPromiseRejectBlock
     ) {
-      banksComponent?.updateCollectedData(collectableData: BanksCollectableData.bankId(bankId: bankId))
-      resolver(nil)
+        banksComponent?.updateCollectedData(collectableData: BanksCollectableData.bankId(bankId: bankId))
+        resolver(nil)
     }
-  }
+}
 
-  extension RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManager: PrimerHeadlessSteppableDelegate {
+extension RNTPrimerHeadlessUniversalCheckoutBanksComponent: PrimerHeadlessSteppableDelegate {
     func didReceiveStep(step: PrimerSDK.PrimerHeadlessStep) {
-      guard let step = step as? BanksStep else { return }
-      switch step {
-      // Handle bank list loading being in progress (e.g. show loading indicator)
-      case .loading:
-          self.sendEvent(
-                       withName: RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.onRetrieving.stringValue,
-                       body: ["retrieving": true])
-        break
-      // Display list of banks
-      case .banksRetrieved(banks: let banks):
-        guard var data = try? JSONEncoder().encode(banks) else { return  }
-                      var json = try? JSONSerialization.jsonObject(with: data)
-        
-          self.sendEvent(
-            withName: RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.onRetrieved.stringValue,
-            body: ["banks": json])
-        break
-      }
+        guard let step = step as? BanksStep else { return }
+        switch step {
+        case .loading: // Handle bank list loading being in progress (e.g. show loading indicator)
+            let rnLoading = try? step.toLoadingRN().toJsonObject()
+            
+            sendEvent(
+                withName: PrimerHeadlessUniversalCheckoutComponentEvent.onStep.stringValue,
+                body: rnLoading
+            )
+        case .banksRetrieved(banks: let banks): // Display list of banks
+            let rnBanks = try? step.toBanksRetrievedRN(banks: banks).toJsonObject()
+
+            sendEvent(
+                withName: PrimerHeadlessUniversalCheckoutComponentEvent.onStep.stringValue,
+                body: rnBanks)
+        }
     }
-  }
-extension RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManager: PrimerHeadlessValidatableDelegate {
+}
+
+extension RNTPrimerHeadlessUniversalCheckoutBanksComponent: PrimerHeadlessValidatableDelegate {
     func didUpdate(validationStatus: PrimerSDK.PrimerValidationStatus, for data: PrimerSDK.PrimerCollectableData?) {
         guard let data = data as? BanksCollectableData else { return }
         
         let eventName: String
         switch validationStatus {
         case .valid:
-            eventName = RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.onValid.stringValue
+            eventName = PrimerHeadlessUniversalCheckoutComponentEvent.onValid.stringValue
         case .validating:
-            eventName = RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.onValidating.stringValue
+            eventName = PrimerHeadlessUniversalCheckoutComponentEvent.onValidating.stringValue
         case .invalid(let errors):
-            let jsonErrors = try? errors.toJsonObject()
+            let rnErrors = errors.map { $0.toPrimerValidationErrorRN() }
+            let serializedData = try? ["errors": rnErrors].toJsonObject()
+            
             self.sendEvent(
-                withName: RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.onInvalid.stringValue,
-                body: ["errors": jsonErrors])
+                withName: PrimerHeadlessUniversalCheckoutComponentEvent.onInvalid.stringValue,
+                body: serializedData)
             return
         case .error(let error):
-            let nsError = NSError(domain: error.errorId, code: -1, userInfo: error.errorUserInfo)
+            let rnError = error.toPrimerErrorRN()
+            let serializedData = try? ["errors": [rnError]].toJsonObject()
+            
             self.sendEvent(
-                withName: RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.onError.stringValue,
-                body: ["errors": error])
+                withName: PrimerHeadlessUniversalCheckoutComponentEvent.onValidationError.stringValue,
+                body: serializedData)
             return
         }
-        
+
+        var serializedData: Any?
         switch data {
         case .bankId(let bankId):
-            sendEventData(eventName, body: ["id": "\(bankId)"])
+            let rnBankId = data.toBankIdRN(bankId: bankId)
+            serializedData = try? ["data": rnBankId].toJsonObject()
         case .bankFilterText(let text):
-            sendEventData(eventName, body: ["text": "\(text)"])
+            let rnText = data.toFilterRN(text: text)
+            serializedData = try? ["data": rnText].toJsonObject()
         }
-    }
-    
-    private func sendEventData<T: Encodable>(_ eventName: String, body: T) {
-        let jsonBody = try? body.toJsonObject()
-        self.sendEvent(withName: eventName, body: ["data": jsonBody])
+
+        sendEvent(withName: eventName, body: serializedData)
     }
 }
 
-extension RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManager: PrimerHeadlessErrorableDelegate {
+extension RNTPrimerHeadlessUniversalCheckoutBanksComponent: PrimerHeadlessErrorableDelegate {
     func didReceiveError(error: PrimerSDK.PrimerError) {
-        let error = NSError(domain: error.errorId, code: -1, userInfo: error.errorUserInfo)
+        let rnError = error.toPrimerErrorRN()
+        let serializedData = try? ["errors": [rnError]].toJsonObject()
         
         self.sendEvent(
-            withName: RNTPrimerHeadlessUniversalCheckoutComponentWithRedirectManagerEvents.onError.stringValue,
-            body: ["errors": error])
+            withName: PrimerHeadlessUniversalCheckoutComponentEvent.onError.stringValue,
+            body: serializedData)
     }
 }
+
