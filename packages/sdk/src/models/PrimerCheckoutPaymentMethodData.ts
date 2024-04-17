e09bd6c0d5610c66e1059d08b6517d4f191dad8d
Niall Quinn <niall@primer.io>
2024-04-17 11:52:14 +0200

    Deprecate paymentMethodType, introduce paymentMethod

--- packages/sdk/src/models/PrimerCheckoutPaymentMethodData.ts
+++ packages/sdk/src/models/PrimerCheckoutPaymentMethodData.ts
@@ -1,5 +1,9 @@
 export type PrimerCheckoutPaymentMethodData = IPrimerCheckoutPaymentMethodData;
 
 interface IPrimerCheckoutPaymentMethodData {
+    /**
+    * @deprecated The method should not be used
+    */
     paymentMethodType: string;
+    paymentMethod: string;
 }
