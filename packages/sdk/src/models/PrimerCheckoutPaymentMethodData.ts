470034930a126f74d16807c786ee77a58c0ad740
Niall Quinn <niall@primer.io>
2024-04-17 12:48:25 +0200

    Update PrimerCheckoutPaymentMethodData.ts

--- packages/sdk/src/models/PrimerCheckoutPaymentMethodData.ts
+++ packages/sdk/src/models/PrimerCheckoutPaymentMethodData.ts
@@ -1,7 +1,9 @@
 export type PrimerCheckoutPaymentMethodData = IPrimerCheckoutPaymentMethodData;
 
 interface IPrimerCheckoutPaymentMethodData {
+    /*
     * @deprecated use paymentMethod field instead
+    */
     paymentMethodType: string;
     
     paymentMethod: string;
