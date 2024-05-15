7ce6d4ad4e70d0f90e5b9563d42838585b624822
Semir <semir@primer.io>
2024-05-15 15:34:38 +0200

    ci: collect coverage from TS files and upload to Sonar

--- packages/sdk/jest.config.js
+++ packages/sdk/jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    testRegex: '/__tests__/.*\\.test\\.[j|t]s$',
+    collectCoverageFrom: ['src/**/{!(hooks|manifest),}.ts'],
+    collectCoverage: true,
+    coveragePathIgnorePatterns: ['node_modules', '.module.ts', '.mock.ts'],
     transform: {
-      '^.+\\.tsx?$': 'ts-jest',
+      '\\.[jt]s$': 'ts-jest',
+      '\\.[jt]sx$': 'ts-jest',
     },
   };
