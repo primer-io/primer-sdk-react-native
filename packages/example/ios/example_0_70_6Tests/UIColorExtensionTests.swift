924169dd831601d902f93d33d52da9819925b361
Niall Quinn <niall@primer.io>
2024-05-16 11:48:33 +0200

    Add latest UIColor Tests

--- packages/example/ios/example_0_70_6Tests/UIColorExtensionTests.swift
+++ packages/example/ios/example_0_70_6Tests/UIColorExtensionTests.swift
@@ -1,5 +1,5 @@
 //
-//  ErrorExtensionTests.swift
+//  UIColorExtensionTests.swift
 //  example_0_70_6Tests
 //
 //  Created by Niall Quinn on 16/05/24.
@@ -8,28 +8,28 @@
 import XCTest
 @testable import primer_io_react_native
 
-final class ErrorExtensionTests: XCTestCase {
+final class UIColorExtensionTests: XCTestCase {
 
-  func test_rnError() throws {
-    // Test case 1: Error is PrimerValidationError
-            let primerValidationError = PrimerV
-            var errorDict = primerValidationError.rnError
-            XCTAssertEqual(errorDict["errorId"], "123")
-            XCTAssertEqual(errorDict["diagnosticsId"], "456")
-            XCTAssertEqual(errorDict["description"], "Primer Validation Error")
+  func test_toHex() throws {
+    // Tesddt case 1: RGB color without alpha
+    let color1 = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
+    XCTAssertEqual(color1.toHex(), "000000")
 
-            // Test case 2: Error is PrimerError
-            let primerError = PrimerError(errorId: "789", diagnosticsId: "012", description: "Primer Error")
-            errorDict = primerError.rnError
-            XCTAssertEqual(errorDict["errorId"], "789")
-            XCTAssertEqual(errorDict["diagnosticsId"], "012")
-            XCTAssertEqual(errorDict["description"], "Primer Error")
+    let color2 = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+    XCTAssertEqual(color2.toHex(), "FFFFFF")
 
-            // Test case 3: Error is NSError
-            let nsError = NSError(domain: "com.example.error", code: 404, userInfo: ["inputElementType": "textField"])
-            errorDict = nsError.rnError
-            XCTAssertEqual(errorDict["errorId"], "com.example.error")
-            XCTAssertEqual(errorDict["inputElementType"], "textField")
+    let color3 = UIColor(red: 1, green: 0, blue: 0, alpha: 1.0)
+    XCTAssertEqual(color3.toHex(), "FF0000")
+
+    let color4 = UIColor(red: 0, green: 1, blue: 0, alpha: 1.0)
+    XCTAssertEqual(color4.toHex(), "00FF00")
+
+    let color5 = UIColor(red: 0, green: 0, blue: 1, alpha: 1.0)
+    XCTAssertEqual(color5.toHex(), "0000FF")
+
+    let noAlpha = UIColor(red: 0, green: 0, blue: 1, alpha: 1.0)
+    XCTAssertEqual(noAlpha.toHex(alpha: true), "0000FFFF")
+
   }
 
 }
