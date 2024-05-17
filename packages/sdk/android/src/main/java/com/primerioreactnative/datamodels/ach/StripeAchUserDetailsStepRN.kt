d486ed111617d022beffda1c2fe01a0c3aae8c03
Flaviu Dunca <flaviu@primer.io>
2024-05-17 17:16:32 +0300

    chore: implement StripeAchUserDetailsStep

---
+++ packages/sdk/android/src/main/java/com/primerioreactnative/datamodels/ach/StripeAchUserDetailsStepRN.kt
@@ -0,0 +1,22 @@
+package com.primerioreactnative.datamodels.ach
+
+import kotlinx.serialization.Serializable
+import com.primerioreactnative.datamodels.NamedComponentStep
+import com.primerioreactnative.datamodels.PrimerIssuingBankRN
+
+@Serializable
+internal sealed interface StripeAchUserDetailsStepRN : NamedComponentStep {
+    @Serializable
+    class TokenizationStartedRN : StripeAchUserDetailsStepRN {
+        override val stepName: String = "tokenizationStarted"
+    }
+
+    @Serializable
+    data class CollectUserDetailsRN(
+        val firstName: String,
+        val lastName: String,
+        val emailAddress: String
+    ) : StripeAchUserDetailsStepRN {
+        override val stepName: String = "collectUserDetails"
+    }
+}
---
+++ packages/sdk/android/src/main/java/com/primerioreactnative/datamodels/extensions/ach/StripeAchUserDetailsStep.kt
@@ -0,0 +1,14 @@
+package com.primerioreactnative.datamodels.extensions.ach
+
+import com.primerioreactnative.datamodels.ach.StripeAchUserDetailsStepRN
+import io.primer.android.components.presentation.paymentMethods.nativeUi.stripe.ach.composable.StripeAchUserDetailsStep
+
+internal fun StripeAchUserDetailsStep.TokenizationStarted.toTokenizationStartedRN() =
+    StripeAchUserDetailsStepRN.TokenizationStartedRN()
+
+internal fun StripeAchUserDetailsStep.CollectUserDetails.toCollectUserDetailsRN() =
+    StripeAchUserDetailsStepRN.CollectUserDetailsRN(
+        firstName = firstName,
+        lastName = lastName,
+        emailAddress = emailAddress
+    )
