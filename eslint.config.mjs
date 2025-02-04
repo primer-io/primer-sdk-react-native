77cc7137411ac668676ca28620b0dbbcbae101fa
FlaviuExtPrimer <flaviu@primer.io>
2025-02-04 22:31:33 +0200

    chore: fix lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,7 +23,7 @@ export default tseslint.config(
         rules: {
             '@typescript-eslint/no-shadow': 'error',
             'no-undef': 'off',
-            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/no-unused-vars': ['error', { "argsIgnorePattern": "^_" }],
             'no-unused-vars': 'off',
             'no-async-promise-executor': 'off'
         },
--- packages/example/metro.config.js
+++ packages/example/metro.config.js
@@ -1,4 +1,4 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
 const blacklist = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
