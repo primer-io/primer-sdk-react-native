03a2374eca18e2eb2ecf8e29623fcfcf1cbf73a5
Onur Var <onur.var@primer.io>
2026-04-24 15:01:26 +0300

    feat: add country selector screen and flag/country data (ACC-7226)

--- src/Components/PrimerBillingAddressForm.tsx
+++ src/Components/PrimerBillingAddressForm.tsx
@@ -7,6 +7,7 @@ import { useNavigation } from './internal/navigation/useNavigation';
 import { CheckoutRoute } from './internal/navigation/types';
 import { PrimerTextInput } from './inputs/PrimerTextInput';
 import { CountrySelectorRow } from './inputs/CountrySelectorRow';
+import { getCountryName } from './internal/countries';
 import type { PrimerBillingAddressFormProps, BillingAddressField } from './types/BillingAddressFormTypes';
 
 // Render order of text inputs in the form; first match (reversed) is the last visible field.
@@ -47,6 +48,7 @@ export function PrimerBillingAddressForm({
       {visibleFields.countryCode && (
         <CountrySelectorRow
           value={billingForm.countryCode}
+          displayName={getCountryName(billingForm.countryCode)}
           label={t('primer_card_form_label_country')}
           placeholder={t('primer_card_form_placeholder_country_code')}
           onPress={handleCountryPress}
